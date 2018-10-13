79033c7afc3fdc52fae83550eea571b6a82af2ff
Gabriel <gabrielruss88@gmail.com>
2018-10-13 17:02:02 -0400

    day 2 complete

--- components/Nav.js
+++ components/Nav.js
@@ -2,11 +2,11 @@ import Link from 'next/link'
 
 const Nav = () => (
   <div>
-    <Link href="/sell">
-      <a>Sell</a>
-    </Link>
     <Link href="/">
       <a>Home</a>
+    </Link>
+    <Link href="/sell">
+      <a>Sell</a>
     </Link>
   </div>
 )
